917a22aeb049d485d284fc8c5f7c2d463fc044a8
BingyuanXu <xubingyuan123@hotmail.com>
2020-08-05 00:10:09 -0500

    Initial Commit

---
+++ project17Flashzilla/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  project17Flashzilla
+//
+//  Created by bingyuan xu on 2020-08-05.
+//  Copyright © 2020 bingyuan xu. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
